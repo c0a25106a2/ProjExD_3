1441edac6eb5aa4ef2d12f63d5a4742066418706
c0a25106a2 <c0a25106a2@edu.teu.ac.jp>
2026-04-28 16:47:04 +0900

    追加機能4 途中

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -3,7 +3,7 @@ import random
 import sys
 import time
 import pygame as pg
-
+import math
 
 WIDTH = 1100  # ゲームウィンドウの幅
 HEIGHT = 650  # ゲームウィンドウの高さ
@@ -56,6 +56,7 @@ class Bird:
         self.img = __class__.imgs[(+5, 0)]
         self.rct: pg.Rect = self.img.get_rect()
         self.rct.center = xy
+        self.dire = (+5, 0) # 演習4 向いている方向（初期値：右）
 
     def change_img(self, num: int, screen: pg.Surface):
         """
@@ -82,6 +83,7 @@ class Bird:
             self.rct.move_ip(-sum_mv[0], -sum_mv[1])
         if not (sum_mv[0] == 0 and sum_mv[1] == 0):
             self.img = __class__.imgs[tuple(sum_mv)]
+            self.dire = tuple(sum_mv) # 【応用4】移動方向に合わせて向きを更新
         screen.blit(self.img, self.rct)
 
 
@@ -94,11 +96,22 @@ class Beam:
         ビーム画像Surfaceを生成する
         引数 bird：ビームを放つこうかとん（Birdインスタンス）
         """
-        self.img = pg.image.load(f"fig/beam.png")
+        self.vx, self.vy = bird.dire  # 演習4 こうかとんの向き
+
+        angle = math.degrees(math.atan2(-self.vy, self.vx))
+        self.img = pg.transform.rotozoom(pg.image.load("fig/beam.png"), angle, 1.0)  # 演習4 回転速度の計算
+
         self.rct = self.img.get_rect()
-        self.rct.centery = bird.rct.centery  # ビームの中心縦座標 = こうかとんの中心縦座標
-        self.rct.left = bird.rct.right  #ビームの左座標 = こうかとんの右座標
-        self.vx, self.vy = +5, 0
+
+        # ビームの初期位置調整
+        self.rct.centerx = bird.rct.centerx + bird.rct.width * self.vx / 5
+        self.rct.centery = bird.rct.centery + bird.rct.height * self.vy / 5
+
+        # self.img = pg.image.load(f"fig/beam.png")
+        # self.rct = self.img.get_rect()
+        # self.rct.centery = bird.rct.centery  # ビームの中心縦座標 = こうかとんの中心縦座標
+        # self.rct.left = bird.rct.right  #ビームの左座標 = こうかとんの右座標
+        # self.vx, self.vy = +5, 0
 
     def update(self, screen: pg.Surface):
         """
@@ -192,6 +205,7 @@ class Explosion:
         """
         self.life -= 1
         screen.blit(self.imgs[self.life // 5 % 4], self.rct)
+
 
 
 def main():
